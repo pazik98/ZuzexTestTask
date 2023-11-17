2e8c5dccec90fda773092e59e08042c8d30b0592
pazik98 <nefedow98@mail.ru>
2023-11-17 13:29:35 +0300

    Init commit

---
+++ src/main/java/ru/pazik98/ZuzexTestTask/ZuzexTestTaskApplication.java
@@ -0,0 +1,13 @@
+package ru.pazik98.ZuzexTestTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuzexTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuzexTestTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/pazik98/ZuzexTestTask/ZuzexTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.pazik98.ZuzexTestTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuzexTestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
